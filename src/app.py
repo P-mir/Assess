5ea9171c4004a166a698a96e324eb3cf69377a45
Patrick Guerin <patrick.guerin@businessdecision.be>
2021-03-20 23:23:38 +0100

    prepare for deployment

--- src/app.py
+++ src/app.py
@@ -44,10 +44,9 @@ def main():
 
     FILE_DIR = os.path.dirname(os.path.abspath(__file__))
     PARENT_DIR = os.path.join(FILE_DIR, os.pardir) 
-    print(os.listdir(PARENT_DIR))
     DIR = os.path.join(PARENT_DIR,'data')
-    #data = pd.read_csv(os.path.join(DIR,'breast_cancer.csv'))
-    data = pd.read_csv('data/breast_cancer.csv')
+    data = pd.read_csv(os.path.join(DIR,'breast_cancer.csv'))
+    #data = pd.read_csv('data/breast_cancer.csv')
 
     uploaded_file = st.file_uploader("Play with the demo or upload your own data (CSV format)", type=['csv'])
     if uploaded_file is not None:
